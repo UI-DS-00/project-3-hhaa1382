a925cfc5f0e38fb0fbc08c5c86c2c449688338db
hhaa1382 <mehranfarhamid82@gmail.com>
2022-11-10 22:39:31 +0330

    calculator class add

---
+++ project/app/src/main/java/project/calculator/Calculator.java
@@ -0,0 +1,146 @@
+package project.calculator;
+
+public class Calculator {
+    private final MyStack<Character> operators=new MyStack<>();
+    private final MyStack<Double> numbers=new MyStack<>();
+
+    public String convertToPostfix(String args) throws IllegalStateException{
+        StringBuilder temp=new StringBuilder();
+
+        for(int i=0;i<args.length();i++){
+            char c=args.charAt(i);
+            if(Character.isDigit(c) || c=='.'){
+                if(temp.length()==0 || !checkParenthesis(temp.toString())){
+                    temp.append("(");
+                }
+                temp.append(c);
+            }
+            else if(c=='('){
+                operators.push(c);
+            }
+            else if(c==')'){
+                if(checkParenthesis(temp.toString())) temp.append(")");
+
+                while(!operators.isEmpty() && operators.top()!='('){
+                    temp.append(operators.pop());
+                }
+
+                if(operators.isEmpty()){
+                    throw new IllegalStateException("error");
+                }
+
+                operators.pop();
+            }
+            else{
+                if(checkParenthesis(temp.toString())) temp.append(")");
+                if(!checkMinus(args,i)){
+                    temp.append("(-1)");
+                    operators.push('*');
+                    continue;
+                }
+                while (!operators.isEmpty() && operators.top()!='(' && priority(c)<=priority(operators.top())){
+                    temp.append(operators.pop());
+                }
+                operators.push(c);
+            }
+        }
+        if(checkParenthesis(temp.toString())){
+            temp.append(")");
+        }
+
+        while (!operators.isEmpty()){
+            temp.append(operators.pop());
+        }
+        return temp.toString();
+    }
+
+    private boolean checkMinus(String args,int index){
+        if(index==0){
+            return false;
+        }
+        else if(Character.isDigit(args.charAt(index-1)) || args.charAt(index-1)==')'){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkParenthesis(String args){
+        return args.length() > 0 && (Character.isDigit(args.charAt(args.length() - 1)) || args.charAt(args.length() - 1)=='.');
+    }
+
+    private int priority(char c){
+        if(c=='^'){
+            return 3;
+        }
+        else if(c=='*' || c=='/'){
+            return 2;
+        }
+        else if(c=='-' || c=='+'){
+            return 1;
+        }
+        return 0;
+    }
+
+    private boolean isOperator(char c){
+        return c == '+' || c == '-' || c == '*' || c == '/' || c=='^' ;
+    }
+
+    public double getAnswer(String args){
+        String postfix=convertToPostfix(args);
+        StringBuilder temp=new StringBuilder();
+
+        for(int i=0;i<postfix.length();i++){
+            char c=postfix.charAt(i);
+            if(isOperator(c) && postfix.charAt(i-1)!='('){
+                Double number1=numbers.pop();
+                Double number2=numbers.pop();
+                double hold;
+
+                if(number1==null || number2==null){
+                    throw new IllegalStateException("error");
+                }
+
+                switch (c) {
+                    case '+' -> {
+                        hold=number2 + number1;
+                        numbers.push(hold);
+                    }
+                    case '-' -> {
+                        hold=number2 - number1;
+                        numbers.push(hold);
+                    }
+                    case '*' -> {
+                        hold=number2 * number1;
+                        numbers.push(hold);
+                    }
+                    case '/' -> {
+                        hold=number2 / number1;
+                        numbers.push(hold);
+                    }
+                    case '^' ->{
+                        hold=power(number2,number1);
+                        numbers.push(hold);
+                    }
+                }
+            }
+            else{
+                if(c!='(' && c!=')'){
+                    temp.append(c);
+                }
+                else if(c==')'){
+                    double number=Double.parseDouble(temp.toString());
+                    numbers.push(number);
+                    temp.delete(0,temp.length());
+                }
+            }
+        }
+        return numbers.pop();
+    }
+
+    private double power(double number,double powNumber){
+        if(powNumber==1){
+            return number;
+        }
+        return power(number,powNumber-1)*number;
+    }
+}
--- project/app/src/main/java/project/calculator/MainActivity.java
+++ project/app/src/main/java/project/calculator/MainActivity.java
@@ -3,12 +3,45 @@ package project.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTextValues(findViewById(R.id.number1_button),"1");
+        setTextValues(findViewById(R.id.number2_button),"2");
+        setTextValues(findViewById(R.id.number3_button),"3");
+        setTextValues(findViewById(R.id.number4_button),"4");
+        setTextValues(findViewById(R.id.number5_button),"5");
+        setTextValues(findViewById(R.id.number6_button),"6");
+        setTextValues(findViewById(R.id.number7_button),"7");
+        setTextValues(findViewById(R.id.number8_button),"8");
+        setTextValues(findViewById(R.id.number9_button),"9");
+        setTextValues(findViewById(R.id.number0_button),"0");
+        setTextValues(findViewById(R.id.dot_button),".");
+        setTextValues(findViewById(R.id.power_button),"^");
+        setTextValues(findViewById(R.id.plus_button),"+");
+        setTextValues(findViewById(R.id.minus_button),"-");
+        setTextValues(findViewById(R.id.divide_button),"/");
+        setTextValues(findViewById(R.id.multiple_button),"*");
+        setTextValues(findViewById(R.id.open_parenthesis_button),"(");
+        setTextValues(findViewById(R.id.close_parenthesis_button),")");
+
+        Button equal=findViewById(R.id.equal_button);
+        Button positiveMinus=findViewById(R.id.positive_minus_button);
+    }
+
+    private void setTextValues(Button button,String value){
+        button.setOnClickListener(e->{
+            String temp=String.valueOf(text.getText());
+            text.setText(temp+value);
+        });
     }
 }
